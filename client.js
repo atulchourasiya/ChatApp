078a38e899ee269ee048f1ca35c540c17114b137
Atul chourasiya <atulchourasiya73@gmail.com>
2023-03-19 18:27:37 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://worried-life-jacket-mite.cyclic.app');
 
 const formInput = document.getElementById('formInput');
 const formName = document.getElementById('formName');

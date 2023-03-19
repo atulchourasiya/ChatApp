26dababbc7b695eff901a210561c2712ec6f1fea
Atul chourasiya <atulchourasiya73@gmail.com>
2023-03-19 21:59:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(function(req,res){
  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Server is up and running!');
   res.end();
 }).listen(PORT);
 
